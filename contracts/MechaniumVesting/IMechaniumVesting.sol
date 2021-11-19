f15b190f3f3fd0c6c5bc65d7d04d29eb1f2c3313
Nicolas Sénécal <35195832+senecolas@users.noreply.github.com>
2021-11-19 13:29:15 +0100

    :recycle: Refacto MechaniumDistribution and rename it MechaniumPresaleDistribution

---
+++ contracts/MechaniumVesting/IMechaniumVesting.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+/**
+ * @dev Mechanim distribution smart contract interface
+ */
+interface IMechaniumVesting {
+    /**
+     * @dev Allocate an amount of tokens to an address ( only allocator role )
+     */
+    function allocateTokens(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Transfers the allocated tokens to an address ( once the distribution has started )
+     */
+    function claimTokens(address account) external returns (bool);
+
+    /**
+     * @dev Transfers the all the allocated tokens to the respective addresses ( once the distribution has started )
+     */
+    function claimTokensForAll() external returns (bool);
+
+    /**
+     * @dev Get balance of allocated tokens of an address
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @return the amount of allocated tokens for `account` from the beginning
+     */
+    function allocatedTokensOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Get pending tokens of an account ( amont / time )
+     */
+    function pendingTokensOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Get unlockable tokens of an address
+     */
+    function unlockableTokens(address account) external view returns (uint256);
+
+    /**
+     * @dev Get released tokens of an address
+     */
+    function releasedTokensOf(address account) external view returns (uint256);
+
+    /**
+     * @return the token IERC20
+     */
+    function token() external view returns (address);
+
+    /**
+     * @return the total token hold by the contract
+     */
+    function tokenBalance() external view returns (uint256);
+
+    /**
+     * @dev Get total tokens supply
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Get total unallocated tokens
+     */
+    function totalUnallocatedTokens() external view returns (uint256);
+}
