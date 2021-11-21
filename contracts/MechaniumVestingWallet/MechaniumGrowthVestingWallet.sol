16630c6922902f8687580d85fc6a4ea4b6fbe6b3
Nicolas Sénécal <35195832+senecolas@users.noreply.github.com>
2021-11-21 10:43:39 +0100

    :sparkles: #2 Growth (marketing) fund lock & vesting

---
+++ contracts/MechaniumVestingWallet/MechaniumGrowthVestingWallet.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "./MechaniumVestingWallet.sol";
+
+/**
+ * @title MechaniumGrowthVestingWallet - $MECHA allocated to the growth and marketing operations with a vesting schedule
+ */
+contract MechaniumGrowthVestingWallet is MechaniumVestingWallet {
+    /**
+     * @dev Contract constructor sets the configuration of the vesting schedule
+     * @param token_ Address of the ERC20 token contract, this address cannot be changed later
+     */
+    constructor(IERC20 token_)
+        MechaniumVestingWallet(
+            token_,
+            40, // Initially unlock 40%
+            15, // then unlock 15%
+            180 days // every 6 months
+        )
+    {}
+}
