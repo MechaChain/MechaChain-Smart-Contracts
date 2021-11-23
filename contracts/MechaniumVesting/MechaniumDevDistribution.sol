e7a01cdfdaf7034ba2002a3a76484c3f156e31fe
Nicolas Sénécal <35195832+senecolas@users.noreply.github.com>
2021-11-23 18:35:21 +0100

    :sparkles: #4 Added MechaniumDevDistribution contract

---
+++ contracts/MechaniumVesting/MechaniumDevDistribution.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./MechaniumTeamDistribution.sol";
+
+/**
+ * @title MechaniumDevDistribution - Vesting and distribution smart contract for the mechachain development team
+ */
+contract MechaniumDevDistribution is MechaniumTeamDistribution {
+    /**
+     * @dev Contract constructor sets the configuration of the vesting schedule
+     * @param token_ Address of the ERC20 token contract, this address cannot be changed later
+     */
+    constructor(IERC20 token_)
+        MechaniumTeamDistribution(
+            token_,
+            0, // at the allocation
+            20, // unlock 20%
+            90 days // and repeat every 3 month
+        )
+    {}
+}
