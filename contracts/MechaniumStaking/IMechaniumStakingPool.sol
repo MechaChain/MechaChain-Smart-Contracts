df23f27901b56dfe7b94ce42d301f52f4308924c
SamAyed <m.ayed.sami@gmail.com>
2022-02-11 13:58:44 +0100

    Issue #19: - Add missing functions in interface - Add doc for interface functions - Add missing doc for staking smart contract

--- contracts/MechaniumStaking/IMechaniumStakingPool.sol
+++ contracts/MechaniumStaking/IMechaniumStakingPool.sol
@@ -23,39 +23,112 @@ interface IMechaniumStakingPool {
         uint64 lockedUntil;
     }
 
+    /**
+     * @notice Used to stake an `amount` of tokens for a `lockPeriod` for the `msg.sender`
+     */
     function stake(uint256 amount, uint64 lockPeriod) external returns (bool);
 
+    /**
+     * @notice Used to stake an `amount` of tokens for a `lockPeriod` for an `account`
+     */
     function stakeFor(
         address account,
         uint256 amount,
         uint64 lockPeriod
     ) external returns (bool);
 
+    /**
+     * @notice Used to update a `depositId`'s `lockPeriod`
+     */
     function updateStakeLock(uint256 depositId, uint64 lockPeriod)
         external
         returns (bool);
 
+    /**
+     * @notice Used to calculate and pay pending rewards to the `msg.sender`
+     */
     function processRewards() external returns (uint256);
 
+    /**
+     * @notice Used to unstake a `depositId` for the `msg.sender`
+     */
     function unstake(uint256 depositId) external returns (bool);
 
+    /**
+     * @notice Used to update the rewards per weight and the total rewards
+     */
     function updateRewards() external returns (bool);
 
+    /**
+     * @notice Used to change the rewardsPerBlock
+     */
     function setRewardsPerBlock(uint256 rewardsPerBlock)
         external
         returns (bool);
 
+    /**
+     * @notice Used to get the remaining allocated tokens
+     */
     function remainingAllocatedTokens() external returns (uint256);
 
+    /**
+     * @notice Used to get the pending rewards for an `account`
+     */
     function pendingRewards(address account) external returns (uint256);
 
+    /**
+     * @notice Can we call the rewards function or is it useless and will cause an error
+     */
+    function canUpdateRewardsPerWeight() external returns (bool);
+
+    /**
+     * @notice Used to get the balance for an `account`
+     */
     function balanceOf(address account) external returns (uint256);
 
+    /**
+     * @notice Used to get the deposit (`depositId`) for an `account`
+     */
     function getDeposit(address account, uint256 depositId)
         external
         returns (Deposit memory);
 
+    /**
+     * @notice Used to get the length of deposits for an `account`
+     */
     function getDepositsLength(address account) external returns (uint256);
 
-    // TODO : add calculateUserWeight
+    /**
+     * @notice Used to get the User data for an `account`
+     */
+    function getUser(address account) external returns (User memory);
+
+    /**
+     * @notice Get the updated rewards
+     */
+    function updatedRewards() external returns (uint256);
+
+    /**
+     * @notice Get the total updated rewards
+     */
+    function updatedTotalRewards() external returns (uint256);
+
+    /**
+     * @notice Get the updated rewards per weight
+     */
+    function updatedRewardsPerWeight() external returns (uint256);
+
+    /**
+     * @notice Calculate the weight based on `amount` and `stakingTime`
+     */
+    function calculateUserWeight(uint256 amount, uint64 stakingTime)
+        external
+        returns (uint256);
+
+    /**
+     * @notice Converts stake weight to reward value, applying the division on weight
+     */
+    function weightToReward(uint256 _weight, uint256 _rewardsPerWeight)
+        external
+        returns (uint256);
 }
--- contracts/MechaniumStaking/MechaniumStakingPool.sol
+++ contracts/MechaniumStaking/MechaniumStakingPool.sol
@@ -126,6 +126,18 @@ contract MechaniumStakingPool is IMechaniumStakingPool, Ownable {
      * ========================
      */
 
+    /**
+     * @notice Contract constructor sets the configuration of the staking pool
+     * @param stakedToken_ The token to be staked ( can be same as rewardToken if not flash pool )
+     * @param rewardToken_  The token to be rewarded
+     * @param initBlock_ The init block ( if set to 0 will take the current block )
+     * @param minStakingTime_ The minimum allowed locking time
+     * @param maxStakingTime_ The maximum allowed locking time
+     * @param minWeightMultiplier_ The minimum weight multipler ( Used to calculate weight range )
+     * @param maxWeightMultiplier_ The maximum weight multipler ( Used to calculate weight range )
+     * @param rewardsLockingPeriod_  The rewards locking period ( Can be 0 if flash pool )
+     * @param rewardsPerBlock_ The amount of tokens to be rewarded per block passed
+     */
     constructor(
         IERC20 stakedToken_,
         IERC20 rewardToken_,
@@ -508,7 +520,7 @@ contract MechaniumStakingPool is IMechaniumStakingPool, Ownable {
     /**
      * @notice Can we call the rewards function or is it useless and will cause an error
      */
-    function canUpdateRewardsPerWeight() public view returns (bool) {
+    function canUpdateRewardsPerWeight() public view override returns (bool) {
         return block.number >= initBlock;
     }
 
@@ -560,7 +572,12 @@ contract MechaniumStakingPool is IMechaniumStakingPool, Ownable {
      * @notice Used to get the User data for an `account`
      * @param account The account address
      */
-    function getUser(address account) public view returns (User memory) {
+    function getUser(address account)
+        public
+        view
+        override
+        returns (User memory)
+    {
         User memory user = users[account];
 
         return user;
@@ -570,7 +587,7 @@ contract MechaniumStakingPool is IMechaniumStakingPool, Ownable {
      * @notice Get the updated rewards
      * @dev Used to calculate the rewards for last period ( in blocks ) without updating them
      */
-    function updatedRewards() public view returns (uint256) {
+    function updatedRewards() public view override returns (uint256) {
         if (block.number < initBlock) {
             return 0;
         }
@@ -603,7 +620,7 @@ contract MechaniumStakingPool is IMechaniumStakingPool, Ownable {
      * @notice Get the total updated rewards
      * @dev Used to calculate the rewards from the init block without updating them
      */
-    function updatedTotalRewards() public view returns (uint256) {
+    function updatedTotalRewards() public view override returns (uint256) {
         uint256 _updatedTotalRewards = totalRewards.add(updatedRewards());
 
         return _updatedTotalRewards;
@@ -613,7 +630,7 @@ contract MechaniumStakingPool is IMechaniumStakingPool, Ownable {
      * @notice Get the updated rewards per weight
      * @dev Used to calculate the rewardsPerWeight without updating them
      */
-    function updatedRewardsPerWeight() public view returns (uint256) {
+    function updatedRewardsPerWeight() public view override returns (uint256) {
         uint256 cumulatedRewards = updatedRewards();
 
         cumulatedRewards = cumulatedRewards.mul(WEIGHT_MULTIPLIER);
@@ -633,6 +650,7 @@ contract MechaniumStakingPool is IMechaniumStakingPool, Ownable {
     function calculateUserWeight(uint256 amount, uint64 stakingTime)
         public
         view
+        override
         returns (uint256)
     {
         return
@@ -659,6 +677,7 @@ contract MechaniumStakingPool is IMechaniumStakingPool, Ownable {
     function weightToReward(uint256 _weight, uint256 _rewardsPerWeight)
         public
         pure
+        override
         returns (uint256)
     {
         return _weight.mul(_rewardsPerWeight).div(WEIGHT_MULTIPLIER);
