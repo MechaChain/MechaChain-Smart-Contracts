b50bae71dfa9a21cdd634fbdef88fa9064717226
Nicolas Sénécal <35195832+senecolas@users.noreply.github.com>
2022-02-25 22:43:39 +0100

    :white_check_mark::zap: scaling tests for staking pool

--- scalingTest/MechaniumStakingPool_test.js
+++ scalingTest/MechaniumStakingPool_test.js
@@ -1,5 +1,7 @@
 // Load modules
 const { time } = require("@openzeppelin/test-helpers");
+const cliProgress = require("cli-progress");
+const web3Utils = require("web3-utils");
 
 // Load artifacts
 const Mechanium = artifacts.require("Mechanium");
@@ -9,19 +11,47 @@ const MechaniumStakingPoolFactory = artifacts.require(
 const MechaniumStakingPool = artifacts.require("MechaniumStakingPool");
 
 // Load utils
-const { getAmount, getRandom, getBN, getBNRange } = require("../utils");
+const {
+  getAmount,
+  getRandom,
+  getBN,
+  getBNRange,
+  gasTracker,
+} = require("../utils");
 
 contract("MechaniumStakingPoolFactory", (accounts) => {
-  let instance, token, mainPool;
+  let instance, token, mainPool, initBlock;
 
-  const costs = [];
   const stakes = [];
+
+  const CONFIG = {
+    stakedAmount: {
+      min: 100,
+      max: 1000,
+    },
+    stakedTimeInDays: {
+      min: 10,
+      max: 100,
+    },
+    stakesPerUsers: {
+      min: 1,
+      max: 3,
+    },
+    blockAdvanceBeforeStake: {
+      min: 10,
+      max: 100,
+    },
+    blockAdvanceBeforeProcessRewards: {
+      min: 50,
+      max: 100,
+    },
+  };
 
   const mainStakingPoolData = {
     allocatedTokens: getAmount(1000000),
     initBlock: 0,
-    minStakingTime: time.duration.days(10),
-    maxStakingTime: time.duration.days(100),
+    minStakingTime: time.duration.days(CONFIG.stakedTimeInDays.min),
+    maxStakingTime: time.duration.days(CONFIG.stakedTimeInDays.max),
     minWeightMultiplier: getBN(1),
     maxWeightMultiplier: getBN(2),
     rewardsLockingPeriod: time.duration.days(90),
@@ -49,48 +79,26 @@ contract("MechaniumStakingPoolFactory", (accounts) => {
   const calculateWeight = (amount, stakingTime) => {
     return amount
       .mul(getWeightMultiplierRange(stakingTime))
-      .div(WEIGHT_MULTIPLIER)
-  }
-
-  async function addCost(action, data) {
-    const tx = await web3.eth.getTransaction(data.tx);
-
-    const price = data.receipt.gasUsed * Number(tx.gasPrice);
-
-    const priceETH = price / (10 ** 18);
-
-    const cost = {
-      action,
-      gasUsed: data.receipt.gasUsed,
-      gasPrice: Number(tx.gasPrice),
-      price,
-      priceETH
-    };
-
-    costs.push(cost);
-  }
+      .div(WEIGHT_MULTIPLIER);
+  };
 
   async function stake(user, amount, duration) {
     await token.transfer(user, amount);
     await token.approve(mainPool.address, amount, { from: user });
-    const txData = await mainPool.stake(
-      amount,
-      duration,
-      { from: user }
-    );
+    const txData = await mainPool.stake(amount, duration, { from: user });
 
+    const stakeId = stakes.filter((stake) => stake.user === user).length;
     stakes.push({
       user,
       amount,
+      stakeId,
       duration,
-      weight: calculateWeight(amount, duration)
+      weight: calculateWeight(amount, duration),
     });
-
-    await addCost('Stake Tokens', txData);
+    await gasTracker.addCost(`Stake Tokens n°${stakeId + 1}`, txData);
 
     return txData;
   }
-
   it("Smart contract should be deployed", async () => {
     instance = await MechaniumStakingPoolFactory.deployed();
     assert(instance.address !== "");
@@ -98,57 +106,99 @@ contract("MechaniumStakingPoolFactory", (accounts) => {
   });
 
   it("Create staking pool from factory", async () => {
-    const txData = await instance.createPool(...Object.values(mainStakingPoolData));
+    const txData = await instance.createPool(
+      ...Object.values(mainStakingPoolData)
+    );
 
-    await addCost('Create Pool', txData);
+    await gasTracker.addCost("Create Pool", txData);
 
     const mainPoolAddr = await instance.registeredPoolsList.call(0);
     mainPool = await MechaniumStakingPool.at(mainPoolAddr);
+    initBlock = await time.latestBlock();
 
     assert(mainPool, "Pool not created");
   });
 
-  it(`${accounts.length - 1} Users should stake tokens in pool`, async () => {
-    for (let i = 1; i < accounts.length; i++) {
-      const amount = getAmount(getRandom(100, 500));
-      const stakeTime = time.duration.days(getRandom(10, 100));
+  it(`${accounts.length} Users should stake tokens in pool`, async () => {
+    // Set progress bar
+    console.log("\tWait while users stake tokens in pool...");
+    const progressBar = new cliProgress.SingleBar(
+      {
+        format:
+          "\tStaking: [{bar}] {percentage}% | {value}/{total} users | ETA: {eta}s | Duration: {duration_formatted}",
+      },
+      cliProgress.Presets.shades_classic
+    );
+    progressBar.start(accounts.length, 0);
+
+    // Foreach users
+    for (let i = 0; i < accounts.length; i++) {
       const user = accounts[i];
 
-      await stake(user, amount, stakeTime);
+      // Launch multiple stakes
+      const stakeNumber = getRandom(
+        CONFIG.stakesPerUsers.min,
+        CONFIG.stakesPerUsers.max
+      );
+      for (let b = 0; b < stakeNumber; b++) {
+        // Advance some blocks
+        const passBlockNumber = getRandom(
+          CONFIG.blockAdvanceBeforeStake.min,
+          CONFIG.blockAdvanceBeforeStake.max
+        );
+        const latestBlock = await time.latestBlock();
+        await time.advanceBlockTo(latestBlock.add(getBN(passBlockNumber)));
 
-      for (let b = 0; b <= getRandom(0, 3); b++) {
-        await time.advanceBlock();
+        const amount = getAmount(
+          getRandom(CONFIG.stakedAmount.min, CONFIG.stakedAmount.max)
+        );
+        const stakeTime = time.duration.days(
+          getRandom(CONFIG.stakedTimeInDays.min, CONFIG.stakedTimeInDays.max)
+        );
+        await stake(user, amount, stakeTime);
       }
+
+      // Progress
+      progressBar.increment();
     }
-  });
 
-  it("Check weight", async () => {
-    const totalCalculatedWeight = stakes.reduce((prev, curr) => {
-      prev = prev.add(curr.weight);
-      return prev;
-    }, getBN(0));
-
-    const totalPoolWeight = await mainPool.totalUsersWeight();
-
-    assert.equal(
-      totalCalculatedWeight.toString(),
-      totalPoolWeight.toString(),
-      "Wrong total users weight"
-    );
+    progressBar.stop();
   });
 
   it("Process the rewards", async () => {
-    await time.advanceBlock();
-    await time.advanceBlock();
-    await time.advanceBlock();
-    await time.advanceBlock();
+    // Advance 10 blocks
+    await Promise.all(Array(10).fill(time.advanceBlock()));
 
-    for (let i = 1; i < accounts.length; i++) {
+    // Set progress bar
+    console.log("\tWait while users process their rewards...");
+    const progressBar = new cliProgress.SingleBar(
+      {
+        format:
+          "\tProcess rewards: [{bar}] {percentage}% | {value}/{total} users | ETA: {eta}s | Duration: {duration_formatted}",
+      },
+      cliProgress.Presets.shades_classic
+    );
+    progressBar.start(accounts.length, 0);
+
+    // Process rewards
+    for (let i = 0; i < accounts.length; i++) {
+      // Advance some blocks
+      const passBlockNumber = getRandom(
+        CONFIG.blockAdvanceBeforeProcessRewards.min,
+        CONFIG.blockAdvanceBeforeProcessRewards.max
+      );
+      const latestBlock = await time.latestBlock();
+      await time.advanceBlockTo(latestBlock.add(getBN(passBlockNumber)));
+
       const user = accounts[i];
       const txData = await mainPool.processRewards({ from: user });
 
-      await addCost('Process Rewards', txData);
+      await gasTracker.addCost("Process Rewards", txData);
+
+      progressBar.increment();
     }
+
+    progressBar.stop();
   });
 
   it("Change reward per block", async () => {
@@ -156,7 +206,7 @@ contract("MechaniumStakingPoolFactory", (accounts) => {
 
     const poolOldRemaining = await mainPool.remainingAllocatedTokens();
 
-    const newRewardsPerBlock = poolOldRemaining.add(amount).div(getBN(20));
+    const newRewardsPerBlock = poolOldRemaining.add(amount).div(getBN(100)); // Pool is empty in 100 blocks
     amount = amount.add(newRewardsPerBlock.div(getBN(4)));
 
     const txData = await instance.addAllocatedTokens(
@@ -164,85 +214,112 @@ contract("MechaniumStakingPoolFactory", (accounts) => {
       amount,
       newRewardsPerBlock
     );
-    await addCost('Change reward per block', txData);
   });
 
-  it("Reprocess the rewards", async () => {
+  it("Reprocess the rewards after the pool is empty", async () => {
     await time.increase(mainStakingPoolData.maxStakingTime);
     const latestBlock = await time.latestBlock();
-    await time.advanceBlockTo(latestBlock.add(getBN(20)));
+    await time.advanceBlockTo(latestBlock.add(getBN(100))); // 100 blocks after, the pool is empty
 
-    for (let i = 1; i < accounts.length; i++) {
+    // Set progress bar
+    console.log("\tWait while users process their rewards...");
+    const progressBar = new cliProgress.SingleBar(
+      {
+        format:
+          "\tProcess rewards: [{bar}] {percentage}% | {value}/{total} users | ETA: {eta}s | Duration: {duration_formatted}",
+      },
+      cliProgress.Presets.shades_classic
+    );
+    progressBar.start(accounts.length, 0);
+
+    // Process rewards
+    for (let i = 0; i < accounts.length; i++) {
       const user = accounts[i];
       const txData = await mainPool.processRewards({ from: user });
 
-      await addCost('Process Rewards', txData);
+      await gasTracker.addCost("Process Rewards", txData);
+
+      progressBar.increment();
     }
+
+    progressBar.stop();
   });
 
   it("Unstake tokens for all users", async () => {
     await time.increase(mainStakingPoolData.maxStakingTime);
     await time.advanceBlock();
 
-    for (let i = 1; i < accounts.length; i++) {
+    // Set progress bar
+    console.log("\tWait while users process their rewards...");
+    const progressBar = new cliProgress.SingleBar(
+      {
+        format:
+          "\tProcess rewards: [{bar}] {percentage}% | {value}/{total} users | ETA: {eta}s | Duration: {duration_formatted}",
+      },
+      cliProgress.Presets.shades_classic
+    );
+    progressBar.start(accounts.length, 0);
+
+    // Unstake
+    for (let i = 0; i < accounts.length; i++) {
       const user = accounts[i];
-      const txData = await mainPool.methods["unstake(uint256[])"]([0, 1, 2], { from: user });
-      await addCost('Unstake Deposit', txData);
+      const userDepositsLength = await mainPool.getDepositsLength(user);
+      const depositIds = [...Array(userDepositsLength.toNumber()).keys()];
+
+      const txData = await mainPool.methods["unstake(uint256[])"](depositIds, {
+        from: user,
+      });
+      await gasTracker.addCost(`Unstake ${depositIds.length} Deposits`, txData);
+
+      progressBar.increment();
     }
 
+    progressBar.stop();
+  });
+
+  it("The pool must no longer have any tokens (>1 000 $MECHA) and totalUsersWeight and totalTokensStaked are 0", async () => {
+    await time.advanceBlock();
+    await time.advanceBlock();
+    await time.advanceBlock();
+    await time.advanceBlock();
+    await time.advanceBlock();
+    await time.advanceBlock();
+
+    const poolBalance = await token.balanceOf(mainPool.address);
     const totalUsersWeight = await mainPool.totalUsersWeight();
     const totalTokensStaked = await mainPool.totalTokensStaked();
 
-    console.log("totalUsersWeight :", totalUsersWeight.toString());
-    console.log("totalTokensStaked :", totalTokensStaked.toString());
-    const poolBalance = await token.balanceOf(mainPool.address);
-    console.log("poolBalance :", poolBalance.toString());
-    console.log("Tokens loss :", (poolBalance.toNumber() / (10 ** 18)));
+    assert.equal(
+      totalTokensStaked.toString(),
+      "0",
+      "Incorrect totalTokensStaked"
+    );
+    assert.equal(
+      totalUsersWeight.toString(),
+      "0",
+      "Incorrect totalUsersWeight"
+    );
+
+    const poolBalanceOnMecha = web3Utils.fromWei(poolBalance, "ether");
+    console.log(
+      "\tTokens loss at the end of the pool :",
+      poolBalanceOnMecha,
+      " $MECHA"
+    );
+    assert.equal(
+      poolBalance.cmp(getAmount(1000)),
+      -1,
+      "Incorrect pool balance (must not exceed 1 000 $MECHA of loss)"
+    );
+
+    const latestBlock = await time.latestBlock();
+    console.log(
+      "\tNumber of blocks passed:",
+      latestBlock.sub(initBlock).toString()
+    );
   });
 
-  /* it("Get stats on gas", async () => {
-    console.log("<====== Avg. per Action ======>");
-    const totalStats = costs.reduce((prev, curr) => {
-      // Init
-      prev[curr.action] = prev[curr.action] || {
-        totalCalls: 0,
-        totalGasUsed: 0,
-        totalPrice: 0,
-        totalPriceETH: 0,
-        avgPriceETH: 0,
-        minGasUsed: curr.gasUsed,
-        minPriceETH: curr.priceETH,
-        maxGasUsed: 0,
-        maxPriceETH: 0,
-      };
-
-      // Calcul
-      const value = prev[curr.action];
-
-      value.totalCalls++;
-      value.totalGasUsed += curr.gasUsed;
-      value.totalPrice += curr.price;
-      value.totalPriceETH += curr.priceETH;
-      value.avgPriceETH = value.totalPriceETH / value.totalCalls;
-
-      if (curr.gasUsed < value.minGasUsed) {
-        value.minGasUsed = curr.gasUsed;
-        value.minPriceETH = curr.priceETH;
-      }
-
-      if (curr.gasUsed > value.maxGasUsed) {
-        value.maxGasUsed = curr.gasUsed;
-        value.maxPriceETH = curr.priceETH;
-      }
-
-      return prev;
-    }, {});
-
-    Object.keys(totalStats).forEach((key) => {
-      console.log(`======> Action : ${key}`);
-      Object.keys(totalStats[key]).forEach((_key) => {
-        console.log(`${_key} :`, totalStats[key][_key]);
-      });
-    });
-  }); */
+  it("Get stats on gas", async () => {
+    gasTracker.consoleStats();
+  });
 });
--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,6 @@
 // Load modules
 const { BN } = require("@openzeppelin/test-helpers");
+
 const fs = require("fs");
 
 let deployedContractsList = require("../DEPLOYED_CONTRACTS.json");
@@ -58,9 +59,102 @@ const getBNRange = (x1, y1, x2, y2, a) =>
 
 const getRandom = (min = 0, max = 100) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
+
+// gasTracker;
+const costs = [];
+let gasPrice = 0;
+const addCost = async (action, data) => {
+  const tx = await web3.eth.getTransaction(data.tx);
+  gasPrice = tx.gasPrice;
+  const price = data.receipt.gasUsed * Number(gasPrice);
+
+  const priceETH = web3.utils.fromWei(`${price}`, "ether");
+
+  const cost = {
+    action,
+    gasUsed: data.receipt.gasUsed,
+    gasPrice: gasPrice,
+    price,
+    priceETH,
+  };
+  costs.push(cost);
+};
+const getStats = () => {
+  const stats = costs.reduce((prev, curr) => {
+    // Init
+    prev[curr.action] = prev[curr.action] || {
+      totalCalls: 0,
+      totalGasUsed: 0,
+      avgGasUsed: 0,
+      totalPrice: 0,
+      totalPriceETH: 0,
+      avgPriceETH: 0,
+      minGasUsed: curr.gasUsed,
+      minPriceETH: curr.priceETH,
+      maxGasUsed: 0,
+      maxPriceETH: 0,
+    };
+
+    // Calcul
+    const value = prev[curr.action];
+
+    value.totalCalls++;
+    value.totalGasUsed += curr.gasUsed;
+    value.totalPrice += curr.price;
+    value.totalPriceETH = web3.utils.fromWei(`${value.totalPrice}`, "ether");
+    value.avgPriceETH = web3.utils.fromWei(
+      `${Math.round(value.totalPrice / value.totalCalls)}`,
+      "ether"
+    );
+    value.avgGasUsed = Math.round(value.totalGasUsed / value.totalCalls);
+
+    if (curr.gasUsed < value.minGasUsed) {
+      value.minGasUsed = curr.gasUsed;
+      value.minPriceETH = curr.priceETH;
+    }
+
+    if (curr.gasUsed > value.maxGasUsed) {
+      value.maxGasUsed = curr.gasUsed;
+      value.maxPriceETH = curr.priceETH;
+    }
+
+    return prev;
+  }, {});
+
+  return Object.keys(stats)
+    .sort()
+    .reduce((r, k) => ((r[k] = stats[k]), r), {}); // sort stats by index
+};
+const consoleStats = () => {
+  console.log("\n\t================== GAS STATS ==================");
+
+  console.log(
+    `\t=> Network Gas Price = ${web3.utils.fromWei(
+      getBN(gasPrice),
+      "gwei"
+    )} Gwei`
+  );
+
+  console.table(getStats(), [
+    "totalCalls",
+    "totalGasUsed",
+    "avgGasUsed",
+    //"totalPriceETH",
+    "avgPriceETH",
+    "minGasUsed",
+    "minPriceETH",
+    "maxGasUsed",
+    "maxPriceETH",
+  ]);
+};
 
 module.exports = {
+  gasTracker: {
+    addCost,
+    getStats,
+    consoleStats,
+  },
   getAmount,
   getBN,
   getDeployedContract,
