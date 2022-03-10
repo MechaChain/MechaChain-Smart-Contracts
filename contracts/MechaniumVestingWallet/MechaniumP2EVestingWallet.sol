b00b4d6a23b5dfbebb16a9d31e0d77b89d71c156
thibautvdu <thibautdumont@pm.me>
2022-03-10 17:34:08 +0100

    Adding a dedicated vesting wallet for the P2E pool

---
+++ contracts/MechaniumVestingWallet/MechaniumP2EVestingWallet.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "./MechaniumVestingWallet.sol";
+
+/**
+ * @title MechaniumGrowthVestingWallet - Hold $MECHA allocated to the P2E pool with a vesting schedule
+ * @author EthernalHorizons - <https://ethernalhorizons.com/>
+ * @custom:project-website  https://mechachain.io/
+ * @custom:security-contact contracts@ethernalhorizons.com
+ */
+contract MechaniumP2EVestingWallet is MechaniumVestingWallet {
+    
+    /**
+     * @dev Contract constructor sets the configuration of the vesting schedule
+     * @param token_ Address of the ERC20 token contract, this address cannot be changed later
+     */
+    constructor(IERC20 token_)
+        MechaniumVestingWallet(
+            token_,
+            0, // Initially unlock 15%
+            50, // then unlock 15%
+            270 days // every 9 months
+        )
+    {}
+}
