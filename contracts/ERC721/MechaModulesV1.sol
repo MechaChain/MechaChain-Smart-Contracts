e7d2018638715fc056ec55ba57d85e272020565e
NatashkinAlex <natashkin.a@gameplaygalaxy.com>
2024-03-19 17:39:23 +0700

    add ERC721BurnableUpgradeable in MechaModulesV1

--- contracts/ERC721/MechaModulesV1.sol
+++ contracts/ERC721/MechaModulesV1.sol
@@ -2,12 +2,15 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract MechaModulesV1 is Initializable, ERC721Upgradeable, AccessControlUpgradeable {
+contract MechaModulesV1 is Initializable, ERC721Upgradeable, ERC721BurnableUpgradeable, AccessControlUpgradeable, UUPSUpgradeable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     uint256 private _nextTokenId;
+    uint256 public version;
     string private uri;
 
     event ChangeBaseURI(string indexed uri, address indexed changer);
@@ -17,18 +20,34 @@ contract MechaModulesV1 is Initializable, ERC721Upgradeable, AccessControlUpgrad
         _disableInitializers();
     }
 
-    function initialize() initializer public {
+    function initialize()
+    initializer public
+    {
         __ERC721_init("MechaModulesV1", "$MECHAMODULES");
+        __ERC721Burnable_init();
         __AccessControl_init();
+        __UUPSUpgradeable_init();
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
+
+        version = 1;
     }
 
-    function safeMint(address to) external onlyRole(MINTER_ROLE) {
+    function safeMint(address to) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
     }
+
+    function _authorizeUpgrade(address newImplementation)
+    internal
+    onlyRole(DEFAULT_ADMIN_ROLE)
+    override
+    {
+        version++;
+    }
+
+    // The following functions are overrides required by Solidity.
 
     function supportsInterface(bytes4 interfaceId)
     public
